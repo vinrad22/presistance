35ba7bc8920c61a48a48cb2d4a9da491c28250fe
vince radlicz <vradlicz6377@stu.d214.org>
2024-08-27 09:23:29 -0500

    Initial Commit

---
+++ presistance/presistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  presistanceApp.swift
+//  presistance
+//
+//  Created by Vincent M. Radlicz on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct presistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
